a64b79f3dd912c23dc2e8d98437c948a4d652c7b
ishansavaliya <ishansavaliya787@gmail.com>
2025-04-13 22:37:41 +0530

    solve Chunks size limit error in building time

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,40 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router")
+            ) {
+              return "vendor-react";
+            }
+
+            if (
+              id.includes("@radix-ui") ||
+              id.includes("lucide-react") ||
+              id.includes("tailwind")
+            ) {
+              return "vendor-ui";
+            }
+
+            if (
+              id.includes("jspdf") ||
+              id.includes("html2canvas") ||
+              id.includes("docx")
+            ) {
+              return "vendor-pdf";
+            }
+
+            return "vendor-other";
+          }
+        },
+      },
+    },
+  },
 });
